ae28a1b3575aafc7ad55995aa45415d107884da3
Martin Ruiz <martinruiz@rappi.com>
2020-02-26 04:10:27 -0600

    Add Tests WIP
    Add dummy data for test cases

---
+++ src/test/kotlin/com/clip/interview/InterviewApplicationTest.kt
@@ -0,0 +1,16 @@
+package com.clip.interview
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+class InterviewApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
